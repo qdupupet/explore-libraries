a7766754b78677ef1d7efa5d0ee5ed481da3cade
qdupupet <QDupupet45@MassMutual.com>
2018-08-13 13:24:25 -0400

    moving files with fs

---
+++ explore-libraries.R
@@ -0,0 +1,30 @@
+## Copy again, using fs ----
+#install.packages('fs')
+library(fs)
+library(tidyverse)
+
+(from_dir <- path_home("Desktop", "day1_s2_explore-libraries"))
+(from_files <- dir_ls(from_dir, glob = "*.R"))
+(to_files <- path_file(from_files))
+(out <- file_copy(from_files, to_files))
+dir_ls()
+dir_info()
+
+## Sidebar: Why does Jenny name files this way? ----
+library(tidyverse)
+ft <- tibble(files = dir_ls(glob = "*.R"))
+ft
+
+ft %>%
+  filter(str_detect(files, "explore"))
+
+ft %>%
+  mutate(files = path_ext_remove(files)) %>%
+  separate(files, into = c("i", "topic", "flavor"), sep = "_")
+
+dirs <- dir_ls(path_home("Desktop"), type = "directory")
+(dt <- tibble(dirs = path_file(dirs)))
+dt %>%
+  separate(dirs, into = c("day", "session", "topic"), sep = "_")
+
+## Principled use of delimiters --> meta-data can be recovered from filename
