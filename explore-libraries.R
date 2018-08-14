d999388ed978f8496fea21ce3c22dd55ddfc6706
qdupupet <QDupupet45@MassMutual.com>
2018-08-14 09:24:26 -0400

    local deletion

--- explore-libraries.R
+++ explore-libraries.R
@@ -3,7 +3,6 @@
 library(fs)
 library(tidyverse)
 library(ggplot2)
-library(cowplot)
 
 (from_dir <- path_home("Desktop", "day1_s2_explore-libraries"))
 (from_files <- dir_ls(from_dir, glob = "*.R"))
